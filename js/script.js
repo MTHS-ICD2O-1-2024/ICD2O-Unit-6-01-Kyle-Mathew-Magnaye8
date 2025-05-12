2e3800776aec7bf2ce47549e11255e0b4d7bf625
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-05-12 12:55:11 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2025 Kyle Matthew Magnaye
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Kyle Matthew Magnaye
+// Created on: Apr 2025
 // This file contains the JS functions for index.html
+
+const randomNumber = Math.floor(Math.random() * 6) + 1
+
+/**
+ * This function checks if the user has selected the random number from above
+ */
+// eslint-disable-next-line no-unused-vars
+function checkNumber() {
+  // input
+  const userNumber = parseInt(document.getElementById('user-number').value)
+
+  // process and output
+  if (userNumber === randomNumber) {
+
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the correct number!</p>'
+  } else {
+    document.getElementById('result').innerHTML =
+      '<p>You have guessed the wrong number!' +
+      '<br/>The correct number was: ' + randomNumber + '</p>'
+  }
+}
+
