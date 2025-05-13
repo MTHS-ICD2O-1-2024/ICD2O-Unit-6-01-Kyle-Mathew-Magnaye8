bc50f5f897c3b6cc9798edb77408a549adf8523e
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-05-13 03:14:23 +0000

    revamped 1

--- js/script.js
+++ js/script.js
@@ -4,15 +4,11 @@
 // Created on: May 2025
 // This file contains the JS functions for index.html
 
-const randomNumber = Math.floor(Math.random() * 6) + 1;
-
+const randomNumber = Math.floor(Math.random() * 6) + 1
 function checkNumber() {
-  const userGuess = parseInt(document.getElementById("user-number").value);
-  const result = document.getElementById("result");
-
-  if (userGuess === randomNumber) {
-    result.innerHTML = "<p>You guessed the correct number!</p>";
-  } else {
-    result.innerHTML = `<p>You guessed wrong!<br/>The correct number was: ${randomNumber}</p>`;
-  }
+  const e = parseInt(document.getElementById("user-number").value),
+    t = document.getElementById("result")
+  e === randomNumber
+    ? (t.innerHTML = "<p>You guessed the correct number!</p>")
+    : (t.innerHTML = `<p>You guessed wrong!<br/>The correct number was: ${randomNumber}</p>`)
 }
