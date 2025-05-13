fa1e3b03d6d62f9b77f82f2761e9dc25ce76d1d9
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2025-05-13 01:34:08 +0000

    saved work 2

--- js/script.js
+++ js/script.js
@@ -4,25 +4,14 @@
 // Created on: Apr 2025
 // This file contains the JS functions for index.html
 
-const randomNumber = Math.floor(Math.random() * 6) + 1
-
-/**
- * This function checks if the user has selected the random number from above
- */
-// eslint-disable-next-line no-unused-vars
+const randomNumber = Math.floor(6 * Math.random()) + 1
 function checkNumber() {
-  // input
-  const userNumber = parseInt(document.getElementById('user-number').value)
-
-  // process and output
-  if (userNumber === randomNumber) {
-
-    document.getElementById('result').innerHTML =
-      '<p>You have guessed the correct number!</p>'
-  } else {
-    document.getElementById('result').innerHTML =
-      '<p>You have guessed the wrong number!' +
-      '<br/>The correct number was: ' + randomNumber + '</p>'
-  }
+  let e = parseInt(document.getElementById("user-number").value)
+  e === randomNumber
+    ? (document.getElementById("result").innerHTML =
+        "<p>You have guessed the correct number!</p>")
+    : (document.getElementById("result").innerHTML =
+        "<p>You have guessed the wrong number!<br/>The correct number was: " +
+        randomNumber +
+        "</p>")
 }
-
